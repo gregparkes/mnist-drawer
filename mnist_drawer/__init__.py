d00882be36919d91a77c171fd44ae9a24dd9bb9e
GregP <gregorymparkes@gmail.com>
2022-12-18 13:17:35 +0000

    updated version, requirements

--- mnist_drawer/__init__.py
+++ mnist_drawer/__init__.py
@@ -1,7 +1,8 @@
 """Main method of MNIST drawer."""
 
-__version__ = "0.2"
+__version__ = "0.3"
 __name__ = "mnist_drawer"
 
 from ._gui_main import MNISTApplication
 from ._fixed_queue import FixedQueue
+from ._model import LeNet
